db08c8cc1e8ad5d25345592490c70ba8ff133e53
Gregorius Yuristama <102383943+gregoriusyuristama@users.noreply.github.com>
2023-08-25 00:07:46 +0700

    Initial Commit

---
+++ TingleADHD Watch App/TingleADHDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TingleADHDApp.swift
+//  TingleADHD Watch App
+//
+//  Created by Gregorius Yuristama Nugraha on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct TingleADHD_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
